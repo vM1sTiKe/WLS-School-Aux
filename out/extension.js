68ec120738d13f68d15a1af5c551632b35fa919d
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2023-03-06 23:09:35 +0000

    .

--- out/extension.js
+++ out/extension.js
@@ -7,9 +7,10 @@ const vscode = require("vscode");
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 async function activate(context) {
+    const usingInWSL = vscode.workspace.name?.includes("WSL") || false;
     const available = vscode.extensions.all.find(ext => ext.id == "ms-vscode-remote.remote-wsl");
     context.subscriptions.push(vscode.commands.registerCommand("wlsschoolaux.compile", async () => {
-        if (!available)
+        if (!available && !usingInWSL)
             return;
         const workSpaces = vscode.workspace.workspaceFolders;
         //verifies if only one workspace being used
@@ -18,11 +19,15 @@ async function activate(context) {
             return;
         }
         const uri = vscode.workspace.workspaceFolders?.[0].uri;
-        if (!uri)
+        if (!uri) {
+            vscode.window.showInformationMessage("No URI");
             return;
+        }
         //returns if no makefile
-        if (!(await vscode.workspace.fs.readDirectory(uri)).find(f => f[0] == "makefile" && f[1] == 1))
+        if (!(await vscode.workspace.fs.readDirectory(uri)).find(f => f[0] == "makefile" && f[1] == 1)) {
+            vscode.window.showInformationMessage("No makefile file");
             return;
+        }
         //create makefile text to concat extras
         const makeFileSTR = [
             ["default:\n\tgcc -Wall -o prog main.c input.c"],
@@ -38,13 +43,14 @@ async function activate(context) {
         }
         //write data into makefile
         vscode.workspace.fs.writeFile(vscode.Uri.from({ scheme: uri.scheme, path: `${uri.path}/makefile` }), Buffer.from(makeFileSTR.map(str => str.join(" ")).join("\n")));
+        vscode.window.showInformationMessage("Done");
     }));
     //search if the WSL extension is available
-    if (!available) {
+    if (!available && !usingInWSL) {
         vscode.window.showInformationMessage("Non executing cause no WSL available");
         return;
     }
-    if (!vscode.workspace.name?.includes("WSL"))
+    if (!usingInWSL)
         vscode.commands.executeCommand("remote-wsl.reopenInWSL");
 }
 exports.activate = activate;
